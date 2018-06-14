eebee68a80e5dbbcaf998fb332c5d2354fe76f38
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-06-15 00:52:13 +0200

    Fix

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 "use strict";
 const Command = require("command");
-const dataMap = new WeakMap;
 
 function cmdHandler() {
+  if (this.enabled) {
+    this.hook = void this.dispatch.unhook(this.hook);
+  }
+  else {
+    this.hook = this.dispatch.hook("S_DESPAWN_NPC", 3, removeAnimation);
+  }
   this.cmd.message(`Death animations ${
-    (this.settings.enabled = !this.settings.enabled) ? "ena" : "disa"
+    (this.enabled = !this.enabled) ? "ena" : "disa"
   }bled`);
 }
 
 function removeAnimation(evt) {
-  if (!dataMap.get(this).enabled || evt.type !== 5) return;
+  if (evt.type !== 5) return;
   evt.type = 1;
   return true;
 }
 
 function NoMoreDeathAnimations(_) {
-  const cmd = Command(_);
-  const settings = { enabled: true };
-  require("./fix-hookonce")(_);
+  const cmd = this.cmd = Command(_);
+  this.enabled = true;
+  this.dispatch = _;
 
-  cmd.add("deathanimations", cmdHandler, { cmd, settings });
+  cmd.add("deathanimations", cmdHandler, this);
 
-  dataMap.set(_.hook("S_DESPAWN_NPC", 3, removeAnimation), settings);
+  this.hook = _.hook("S_DESPAWN_NPC", 3, removeAnimation);
 }
 
 module.exports = NoMoreDeathAnimations;
