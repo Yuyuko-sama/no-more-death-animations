315a14c448accc3ed2efb5bbec86a9db0adb97cf
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-11-09 02:20:59 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Removes the monster's body once it dies and makes loot appear without its
 "jumping" animation.
 
 # commands
-* `!deathanimations`: toggles the module on or off
+* `!deathanimation`: toggles the module on or off
--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 "use strict";
-const Command = require("command");
 
-function cmdHandler() {
-  if (this.enabled) {
-    this.hook = void this.dispatch.unhook(this.hook);
-  }
-  else {
-    this.hook = this.dispatch.hook("S_DESPAWN_NPC", 3, removeAnimation);
-  }
-  this.cmd.message(`Death animations ${
-    (this.enabled = !this.enabled) ? "ena" : "disa"
-  }bled`);
+module.exports = function DeathAnimation(mod) {
+let enabled = true,
+
+	mod.hook("S_DESPAWN_NPC", 3,  event => {
+		if (event.type !== 5 && enabled) return
+		event.type = 1
+		return true
+	}
+	mod.command.add('deathanimation', {
+        $none() {
+            enabled = !enabled;
+            mod.command.message(`death animations ${enabled ? 'en' : 'dis'}abled`)
+        }
+    })
 }
-
-function removeAnimation(evt) {
-  if (evt.type !== 5) return;
-  evt.type = 1;
-  return true;
-}
-
-function NoMoreDeathAnimations(_) {
-  const cmd = this.cmd = Command(_);
-  this.enabled = true;
-  this.dispatch = _;
-
-  cmd.add("deathanimations", cmdHandler, this);
-
-  this.hook = _.hook("S_DESPAWN_NPC", 3, removeAnimation);
-}
-
-module.exports = NoMoreDeathAnimations;
