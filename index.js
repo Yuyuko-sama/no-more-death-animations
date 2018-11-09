2af88affbc03a0b1ff11f2fe44861475477fe671
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-11-09 15:01:51 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-"use strict";
+"use strict"
 
 module.exports = function DeathAnimation(mod) {
-let enabled = true,
+let enabled = true;
 
-	mod.hook("S_DESPAWN_NPC", 3,  event => {
+	mod.hook('S_DESPAWN_NPC', 3,  event => {
 		if (event.type !== 5 && enabled) return
 		event.type = 1
 		return true
-	}
+	})
 	mod.command.add('deathanimation', {
         $none() {
             enabled = !enabled;
